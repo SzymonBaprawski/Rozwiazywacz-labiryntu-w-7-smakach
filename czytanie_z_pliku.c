0d7effebaa08e70dbe35b7be9a848d099997afe4
Piotr Buchman <piotr.buchman@ssp26.pl>
2024-02-28 23:32:58 +0100

    przetestowana dzialajaca (poza mala niedokladnosciia linijka 38 czytanie_z_pliku.c) funkcja wczytujaca dane z pliku maze. dodtakowa funkcja testujaca w komentarzu czytanie_z_pliku.h.

--- czytanie_z_pliku.c
+++ czytanie_z_pliku.c
@@ -1,6 +1,50 @@
 #include "czytanie_z_pliku.h"
 
-maze returnmaze(FILE* plik)
+maze* returnmaze(FILE* plik) //nalzey podac otwarty do czytania plik "r"
 {
-    
+    maze * dane = malloc(sizeof *dane);
+
+    int tmp= 0;
+    int i = 0;
+    dane->m = 0;
+
+    while(tmp != EOF)
+    {
+        tmp = fgetc(plik);
+        switch (tmp)
+        {
+                case 'X':
+                dane->maz[i][dane->m] = 1;
+                i++;
+                break;
+
+                case ' ':
+                dane->maz[i][dane->m] = 0;
+                i++;
+                break;
+
+                case '\n':
+                i = 0;
+                dane->m++;
+                break;
+
+                case 'P':
+                dane->maz[i][dane->m] = 2;
+                i++;
+                break;
+
+                case 'K':
+                dane->maz[i][dane->m] = 3;
+                dane->n = i; // to nie dziala kiedy K nie jest w dolnym prawym roku labiryntu !!!
+                i++;
+                break;
+
+            }
+               }
+    dane->m--;
+    dane->n++;
+
+    fclose(plik);
+
+    return dane;
 }
--- czytanie_z_pliku.h
+++ czytanie_z_pliku.h
@@ -10,5 +10,29 @@ typedef struct{
     int maz[2049][2049]; //labirynt wypelniony "0"-> pusta kratka () ; "1"->sciana(X); "2"-> poczatek (P); "3"-> koniec (K)  
 }maze;
 
-
 #endif
+
+/*int main(int argc, char ** argv)
+{
+        FILE* in = argc >1 ? fopen(argv[1], "r"):NULL;
+
+    if(in == NULL)
+    {
+        fprintf(stderr, "zly input lub nie moge otworzyc");
+        return 1;
+    }
+
+        maze * lab = returnmaze(in);
+
+        for( int i = 0; i < lab->m ; i++)
+        {
+                for(int j = 0; j< lab->n; j++)
+                {
+                        printf("%d", lab->maz[j][i]);
+                }
+        printf("\n");
+        }
+
+        free(lab);
+        return 0;
+}*/
